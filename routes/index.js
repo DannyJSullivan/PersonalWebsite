fbfaaff588611a87f97ef8ecb44404d053a8a2e5
Danny Sullivan <djsully9@live.com>
2020-05-14 11:58:50 -0400

    emails

--- routes/index.js
+++ routes/index.js
@@ -4,17 +4,14 @@ var nodemailer = require('nodemailer');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  // res.render('index', { title: 'Express' });
   res.sendFile("public/pages/index.html", {root: '.'});
 });
 
 router.get('/code', function(req, res, next) {
-  // res.render('index', { title: 'Express' });
   res.sendFile("public/pages/code.html", {root: '.'});
 });
 
 router.get('/experience', function(req, res, next) {
-  // res.render('index', { title: 'Express' });
   res.sendFile("public/pages/experience.html", {root: '.'});
 });
 
@@ -24,6 +21,31 @@ router.get('/contact', function(req, res, next) {
 
 router.get('/email', function(req, res, next) {
 
+
+
+  var transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: 'dannysullivanwebsite@gmail.com',
+      pass: 'djsullivanwebsite'
+    }
+  });
+
+  var mailOptions = {
+    from: 'dannysullivanwebsite@gmail.com',
+    to: 'danielj.sullivan@outlook.com',
+    subject: 'Sending email with node.js',
+    text: 'super easy'
+  };
+
+  transporter.sendMail(mailOptions, function(error, info) {
+    if(error) {
+      console.log(error);
+    }
+    else {
+      console.log('Email sent: ' + info.response);
+    }
+  });
 });
 
 module.exports = router;
